c52d3c1a8eccd34ef232f937961fd8924cba15b8
GVilla <GVilla>
2017-06-23 13:22:29 +0200

    Cambios sellwindow, jlist, combobox

--- SW2Final/src/SellWindow.java
+++ SW2Final/src/SellWindow.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -17,6 +18,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
 
 import java.awt.BorderLayout;
 
@@ -36,14 +38,21 @@ public class SellWindow extends JFrame implements ActionListener{
 	private JTextField idText;
 	
 	private JTextArea totalText;
-	private DefaultListModel modeloLista=new DefaultListModel();
 	JScrollPane scrollpaneLista;
 	
 	private DBConection oDBConection;
 	private Connection cn;
 
 	private JScrollPane scrollPane;
+	
+	private JList listCat;
+	private JList listProd;
+	
+	private DefaultListModel catModel;
+	private DefaultListModel prodModel;
 
+	private JComboBox tipoCombo;
+	
 	/**
 	 * Create the application.
 	 */
@@ -56,55 +65,74 @@ public class SellWindow extends JFrame implements ActionListener{
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(700, 70, 800, 550);
+		frame.setBounds(700, 70, 900, 550);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		frame.setTitle("Ventas");
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
 		
-		idLabel = new JLabel("IDproducto");
-		idLabel.setBounds(80, 110, 120, 20);
-		frame.getContentPane().add(idLabel);
-		
-		idText = new JTextField();
-		idText.setBounds(170, 110, 150, 20);
-		frame.getContentPane().add(idText);
-		
-		addButton = new JButton("Agregar");
-		addButton.setBounds(350, 110, 100, 20);
-		frame.getContentPane().add(addButton);
-		addButton.addActionListener(this);
+//		idLabel = new JLabel("IDproducto");
+//		idLabel.setBounds(80, 80, 120, 20);
+//		frame.getContentPane().add(idLabel);
+//		
+//		idText = new JTextField();
+//		idText.setBounds(170, 80, 150, 20);
+//		frame.getContentPane().add(idText);
+//		
+//		addButton = new JButton("Agregar");
+//		addButton.setBounds(350, 80, 100, 20);
+//		frame.getContentPane().add(addButton);
+//		addButton.addActionListener(this);
 		
 //		productList = new JList();
 //		productList.setBounds(80, 150, 500, 300);
 //		frame.getContentPane().add(productList);
 //		scrollpaneLista = new JScrollPane();
-		//scrollpaneLista.setViewportView(productList);
+//		scrollpaneLista.setViewportView(productList);
 		
 		areaDescripcion = new JTextArea();
 		scrollPane = new JScrollPane(areaDescripcion);
-		scrollPane.setBounds(80, 150, 200, 300);
+		scrollPane.setBounds(80, 120, 200, 300);
 		frame.getContentPane().add(scrollPane);
 		
 		areaPrecio = new JTextArea();
 		scrollPane = new JScrollPane(areaPrecio);
-		scrollPane.setBounds(300, 150, 150, 300);
+		scrollPane.setBounds(300, 120, 150, 300);
 		frame.getContentPane().add(scrollPane);
 		
 		
 		totalText = new JTextArea("Precio Total: ");
-		totalText.setBounds(500, 400, 230, 20);
+		totalText.setBounds(220, 430, 230, 20);
 		frame.getContentPane().add(totalText);
 		
 		buyButton = new JButton("Confirmar");
-		buyButton.setBounds(500, 430, 150, 20);
+		buyButton.setBounds(300, 460, 150, 20);
 		frame.getContentPane().add(buyButton);
 		buyButton.addActionListener(this);
 		
+		listCat = new JList();
+		listCat.setBounds(500, 120, 150, 300);
+		catModel = new DefaultListModel();
+		listCat.setModel(catModel);
+		frame.getContentPane().add(listCat);
+		
+		listProd = new JList();
+		listProd.setBounds(700, 120, 150, 300);
+		prodModel = new DefaultListModel();
+		listProd.setModel(prodModel);
+		frame.getContentPane().add(listProd);
 		
 		oDBConection = new DBConection();
 		cn = oDBConection.connect();
+		
+		String[] cat = {"Partes de arriba", "Partes de abajo", "Calzado", "Accesorios"};
+		
+		tipoCombo = new JComboBox(cat);
+		tipoCombo.setSelectedIndex(0);
+		tipoCombo.setBounds(80, 80, 170, 25);
+		frame.getContentPane().add(tipoCombo);
+		tipoCombo.addActionListener(this);
 		
 		PreparedStatement drop = null;
 		try {
@@ -124,7 +152,7 @@ public class SellWindow extends JFrame implements ActionListener{
 		PreparedStatement createTab = null;
 		PreparedStatement aux = null;
 		try {
-			createTab = cn.prepareStatement("create table compra (idcompra int(11) auto_increment ,descripcion varchar(30),precio float,cantidad int, primary key (idcompra))");
+			createTab = cn.prepareStatement("create table compra (idcompra int(11) auto_increment ,nombre varchar(30), marca varchar(30),precio float,cantidad int, primary key (idcompra))");
 			//aux = cn.prepareStatement("INSERT INTO compra (idcompra) values (1)");
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
@@ -138,6 +166,48 @@ public class SellWindow extends JFrame implements ActionListener{
 		}
 	}
 	
+	private void llenarCat(String c) {
+		// TODO Auto-generated method stub
+		String categorias = c;
+		
+		if(categorias.equals("Partes de arriba")) {
+			catModel.addElement("camisetas");
+			catModel.addElement("sudaderas");
+			catModel.addElement("camisas");
+			catModel.addElement("punto");
+		}
+		if(categorias.equals("Partes de abajo")) {
+			catModel.addElement("jeans");
+			catModel.addElement("pantalones");
+			catModel.addElement("bermudas");
+		}
+		if(categorias.equals("Calzado")) {
+			catModel.addElement("zapatos");
+			catModel.addElement("zapatillas");
+			catModel.addElement("botas");
+		}
+		if(categorias.equals("Accesorios")) {
+			catModel.addElement("relojes");
+			catModel.addElement("pulseras");
+			catModel.addElement("otros");
+		}
+//		String sentenciaSQL = "select * from categorias";
+//		String datos[]= new String[1];
+//		
+//		try {
+//			oDBConection.connect();
+//			Statement st = cn.createStatement();
+//			ResultSet rs = st.executeQuery(sentenciaSQL);
+//			while(rs.next()) {
+//				datos[0] = rs.getString("nombre");
+//				catModel.addElement(datos[0]);
+//			}		
+//		}
+//		catch(Exception e1) {
+//			
+//		}
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		
 		try {
@@ -229,6 +299,12 @@ public class SellWindow extends JFrame implements ActionListener{
 				catch(Exception e4) {
 
 				}
+			}
+			
+			if(e.getSource() == tipoCombo) {
+				catModel.clear();
+				String x = (String) tipoCombo.getSelectedItem();
+				llenarCat(x);
 			}
 		}
 		catch(Exception e3) {
